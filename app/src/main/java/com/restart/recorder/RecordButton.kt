17922ca0b15f9a8e91559896f9d86b6bd86decd0
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-08-01 14:45:49 +0900

    xml에서 사용할 수 있게 AppCompatImageButton(Context, AttributeSet)를 상속받는 RecordButton클래스 만들기 + UI 생성 초기화면까지만

--- app/src/main/java/com/restart/recorder/RecordButton.kt
+++ app/src/main/java/com/restart/recorder/RecordButton.kt
@@ -4,23 +4,25 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageButton
 
+
+//AppCompatImageButton(Context,AttributeSet)을 상속받아 xml에서 사용할 수 있는 RecordButton클래스로 만들기.
 class RecordButton(
     context: Context,
     attrs: AttributeSet
 ): AppCompatImageButton(context, attrs) {
 
-    fun updateIconWithState(state: State){
+    fun updateIconWithState(state: State){ //enum class로 만든 State 인자로 받기
         when(state) {
-            State.BEFORE_RECORDING -> {
+            State.BEFORE_RECORDING -> { //녹음전 상태일 때 보여질 이미지
                 setImageResource(R.drawable.ic_record)
             }
-            State.ON_RECORDING -> {
+            State.ON_RECORDING -> { //녹음 중일때 보여질 이미지
                 setImageResource(R.drawable.ic_stop)
             }
-            State.AFTER_RECORDING -> {
+            State.AFTER_RECORDING -> { // 녹음 종료 후 보여질 이미지
                 setImageResource(R.drawable.ic_play)
             }
-            State.ON_PLAYING -> {
+            State.ON_PLAYING -> { // 녹음 재생시 보여이미 이미지
                 setImageResource(R.drawable.ic_stop)
             }
         }
