e0f2bbf04be950877f3fbf1a004a7e3b33bbe63b
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-08-01 18:23:28 +0900

    레코더 초기화

--- app/src/main/java/com/restart/recorder/MainActivity.kt
+++ app/src/main/java/com/restart/recorder/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.restart.recorder
 
 import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import java.util.jar.Manifest
@@ -13,8 +14,14 @@ class MainActivity : AppCompatActivity() {
     //권한요청시 인자로 들어갈 권한 지정.
     private val requiredPermissions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
 
-    private var state = State.BEFORE_RECORDING
+    private var state = State.BEFORE_RECORDING //Enum class State에서 녹음전 상태를 초기 상태로 설정.
 
+    private var recorder: MediaRecorder? = null //미디어 레코드 변수선언, 사용하지 않을 때 널로 두는 편이 메모리 관리에 효율적. 오디오,비디오 파일은 용량이 크기 때문.
+
+    //녹음하는 오디오파일울 저장할 수 있는 경로 설정하기.
+    private val recordingFilePath: String by lazy {
+        "${externalCacheDir?.absolutePath}/recording.3gp"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -49,6 +56,28 @@ class MainActivity : AppCompatActivity() {
 
     private fun initView(){
         recordButton.updateIconWithState(state)
+    }
+
+    //미디어 레코더 녹음할 수 있는 상태로 초기화, 안드로이드 개발자 문서Recording 따라하기.
+    private fun startRecording(){
+        recorder = MediaRecorder().apply {
+
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            setOutputFile(recordingFilePath)//전역변수로 세팅한 경로
+            prepare() //준비
+        }
+        recorder?.start() //녹음시작
+    }
+
+    //녹음종료
+    private fun stopRecording(){
+        recorder?.run{
+            stop() //정지
+            release() //메모리 해제
+        }
+        recorder = null //레코더 널처리
     }
 
     companion object {
